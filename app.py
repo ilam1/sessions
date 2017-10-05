b83c407955d81ed3aa17936a5c6c6ef7e408ed92
jaspercheung <jcheung6@stuy.edu>
2017-10-04 20:15:50 -0400

    logout

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask (__name__)
 username = "user"
 password = "pwd"
 out = ""
+is_correct = True
 app.secret_key = os.urandom(32)
 
 @app.route("/")
@@ -23,10 +24,13 @@ def correct():
         pass_right = (password == request.args["password"])
         if user_right and pass_right:
             session[username] = password
+            is_correct = True;
             return "Welcome!"
         elif user_right and not pass_right:
+            is_correct = False;
             return "Error: Wrong password"
         else:
+            is_correct = False;
             return "Error: Wrong username"
     except:
         pass
@@ -34,7 +38,12 @@ def correct():
 @app.route("/loggedin")
 def logged_in():
 #    print(request.args)
-    return render_template("loggedin.html", output = correct())
+    return render_template("loggedin.html", output = correct(),good = is_correct )
+
+@app.route("/loggedout")
+def logged_out():
+    session.pop(username) #ending session
+    return render_template("login.html", output = correct()) #redirecting to login
 
 if __name__ == '__main__':
     app.debug = True
