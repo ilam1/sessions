abb0b1503f24743118074efd4a0f36fad5ab97ee
Irene Lam <ilam1@stuy.edu>
2017-10-04 22:09:24 -0400

    fixed errors, stylistic issues, comments

--- app.py
+++ app.py
@@ -1,66 +1,42 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, session, redirect
 import os
 
 app = Flask (__name__)
 
-global the__username = "user"
-global the_password = "pwd"
-out = ""
-is_correct = True
+the_username = "user" #Hardcoded username
+the_password = "pwd"  #Hardcoded password
 app.secret_key = os.urandom(32)
 
 @app.route("/")
 def hello_world():
+    '''
+    If session has a record of the correct username and password input, the user is logged in
+    Otherwise, the login page is displayed
+    '''
     if "username" in session.keys():
        return render_template("loggedin.html", name = session["username"])
-    return render_template("login.html", output = "")
+    return render_template("login.html", message = "")
 
-   ''' print(session)
-    print(username in session)
-    if username in session:
-        return render_template("loggedin.html")
 
-    return render_template("login.html", output = correct())
-'''
-   
-''' 
-def correct():
-    try:
-        user_right = (username == request.args["username"])
-        pass_right = (password == request.args["password"])
-        if user_right and pass_right:
-            session[username] = password
-            is_correct = True;
-            return "Welcome!"
-        elif user_right and not pass_right:
-            is_correct = False;
-            return "Error: Wrong password"
-        else:
-            is_correct = False;
-            return "Error: Wrong username"
-    except:
-        pass
-
-'''
 @app.route("/loggedin")
 def logged_in():
-#    print(request.args)
    input_name = request.args["username"]
    input_pass = request.args["password"]
-   #CHECKING
+
+   #Validation process, what went wrong (if anything)?
    if input_name == the_username:
       if input_pass == the_password:
-         session["username"] = input_name #new sess
+         session["username"] = input_name #Creates a new session
          return render_template("loggedin.html", name = input_name)
       else:
-         return return_template("login.html", output = "Error: Wrong password")
+         return render_template("login.html", message = "Error: Wrong password")
    else:
-      return render_template("login.html", output =  "Error: Wrong username")
+      return render_template("login.html", message =  "Error: Wrong username")
 
 @app.route("/logout")
 def logged_out():
-    session.pop("username") #ending sess
-    return render_template("login.html", output = "") #redirecting to login
+    session.pop("username") #Ends session
+    return redirect("/") #Redirecting to login
 
 if __name__ == '__main__':
     app.debug = True
